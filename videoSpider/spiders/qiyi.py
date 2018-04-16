2db4a1299f14f8cafd50e6767637e10f1e4ebbef
RockJohnson503 <836867547@qq.com>
2018-04-16 16:58:25 +0800

    完成了对综艺细节的解析

--- videoSpider/spiders/qiyi.py
+++ videoSpider/spiders/qiyi.py
@@ -2,7 +2,7 @@
 import os
 import scrapy
 from urllib import parse
-from tools.selenium_spider import get_last
+from tools.selenium_spider import *
 from scrapy.http import Request
 from videoSpider.items import *
 
@@ -10,7 +10,7 @@ from videoSpider.items import *
 class QiyiSpider(scrapy.Spider):
     name = 'qiyi'
     allowed_domains = ['iqiyi.com']
-    start_urls = ['http://list.iqiyi.com/www/1/----------------iqiyi--.html/']
+    start_urls = ['http://list.iqiyi.com/www/6/----------------iqiyi--.html/']
 
     def parse(self, response):
         # 对视频列表的解析
@@ -44,7 +44,6 @@ class QiyiSpider(scrapy.Spider):
         # 处理电视剧的路径
         play_urls = {}
         album_items = response.css(".albumSubTab-wrap .piclist-wrapper ul li")
-        tab_pills = response.css("#widget-tab-3 .selEpisodeTab-wrap ul li").extract()
         for album_item in album_items:
             if not album_item.css(".site-piclist_pic > a i.icon-yugao-new"):
                 url = album_item.css(".site-piclist_pic > a::attr(href)").extract_first("")
@@ -53,15 +52,17 @@ class QiyiSpider(scrapy.Spider):
             else:
                 break
 
+        tab_pills = response.css("#widget-tab-3 .selEpisodeTab-wrap ul li").extract()
         if len(tab_pills) > 1:
-            for i in get_last(response.url, len(tab_pills), os.path.abspath("tools/phantomjs")):
-                play_urls[i[0]] = i[1]
+            for i, k in get_last_tv(response.url, os.path.abspath("tools/phantomjs")):
+                play_urls[i] = k
 
         # 将解析后的数据添加如Item
         item_loader = VideoItemLoader(item=VideospiderItem(), response=response)
+
         item_loader.add_value("play_url", play_urls)
         item_loader.add_value("list_type", response.meta.get("list_type", ""))
-        item_loader.add_css("video_des", "div.episodeIntro div.episodeIntro-brief[data-moreorless='moreinfo'] span::text")
+        item_loader.add_css("video_des", "div.episodeIntro div.episodeIntro-brief[data-moreorless='lessinfo'] span::text")
         item_loader.add_css("video_name", "div.info-intro h1::attr(data-paopao-wallname)")
         item_loader.add_css("spell_name", "div.info-intro h1::attr(data-paopao-wallname)")
         item_loader.add_css("video_addr", ".episodeIntro-area a::text")
@@ -80,6 +81,7 @@ class QiyiSpider(scrapy.Spider):
     def parse_detail_2(self, response):
         # 对电影细节的解析
         item_loader = VideoItemLoader(item=VideospiderItem(), response=response)
+
         item_loader.add_value("play_url", response.url)
         item_loader.add_value("list_type", response.meta.get("list_type", ""))
         item_loader.add_css("video_des", ".partDes::attr(data-description)")
@@ -97,10 +99,37 @@ class QiyiSpider(scrapy.Spider):
 
 
     def parse_detail_3(self, response):
-        # 对电影细节的解析
-        pass
+        # 对综艺细节的解析
+        play_urls = {}
+        album_items = response.css("#albumpic-showall-wrap li")
+        for album_item in album_items:
+            url = album_item.css(".site-piclist_pic > a::attr(href)").extract_first("")
+            num = album_item.css(".mod-listTitle_right::text").extract_first("")
+            play_urls[num] = url
+
+        tab_pills = response.css("#album_pic_paging[style='display:none;']")
+        if len(tab_pills) == 0:
+            for i, k in get_last_variety(response.url, os.path.abspath("tools/phantomjs")):
+                play_urls[i] = k
+
+        item_loader = VideoItemLoader(item=VideospiderItem(), response=response)
+
+        item_loader.add_value("play_url", play_urls)
+        item_loader.add_value("list_type", response.meta.get("list_type", ""))
+        item_loader.add_css("video_des", ".episodeIntro-brief[data-moreorless='lessinfo'] .briefIntroTxt::text")
+        item_loader.add_css("video_name", "h1[itemprop='name'] .info-intro-title::text")
+        item_loader.add_css("spell_name", "h1[itemprop='name'] .info-intro-title::text")
+        item_loader.add_css("video_addr", ".episodeIntro-area a::text")
+        item_loader.add_css("video_type", ".episodeIntro-type a::text")
+        item_loader.add_css("video_time", ".episodeIntro-lang[itemprop='datePublished'] a::text")
+        item_loader.add_css("video_language", ".episodeIntro-lang[itemprop='inLanguage'] a::text")
+        item_loader.add_value("front_image_url", response.meta.get("front_image_url", ""))
+        item_loader.add_css("video_update_time", ".episodeIntro-update span::text")
+
+        video_item = item_loader.load_item()
+        yield video_item
 
 
     def parse_detail_4(self, response):
-        # 对电影细节的解析
+        # 对动漫细节的解析
         pass
