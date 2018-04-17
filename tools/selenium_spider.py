921d3737a714d7f2b7a40e2b6dc344298d42211a
RockJohnson503 <836867547@qq.com>
2018-04-17 11:53:37 +0800

    完成了对动漫的分页处理

--- tools/selenium_spider.py
+++ tools/selenium_spider.py
@@ -22,7 +22,9 @@ def get_last_tv(url, ex_path):
         j = 1
         while True:
             try:
-                urls = browser.find_element_by_css_selector(".piclist-wrapper[data-tab-body='widget-tab-3'] .site-piclist li:nth-child(" + str(j) + ") .site-piclist_info_title a")
+                urls = browser.find_element_by_css_selector(".piclist-wrapper[data-tab-body='widget-tab-3'] "
+                                                            ".site-piclist li:nth-child(" + str(j) + ") "
+                                                            ".site-piclist_info_title a")
             except:
                 break
             if "预告" not in urls.text:
@@ -50,10 +52,43 @@ def get_last_variety(url, ex_path):
         i = 1
         while True:
             try:
-                time = browser.find_element_by_css_selector("#albumpic-showall-wrap li:nth-child(" + str(i) + ") .wrapper-listTitle span")
-                urls = browser.find_element_by_css_selector("#albumpic-showall-wrap li:nth-child(" + str(i) + ") .site-piclist_pic_link")
+                time = browser.find_element_by_css_selector("#albumpic-showall-wrap "
+                                                            "li:nth-child(" + str(i) + ") "
+                                                            ".wrapper-listTitle span")
+                urls = browser.find_element_by_css_selector("#albumpic-showall-wrap "
+                                                            "li:nth-child(" + str(i) + ") "
+                                                            ".site-piclist_pic_link")
             except:
                 break
             yield time.text, urls.get_attribute("href")
             i += 1
+    browser.quit()
+
+# 动漫的分页处理
+def get_last_anime(url, ex_path):
+    browser = webdriver.PhantomJS(executable_path=ex_path)
+    browser.get(url)
+
+    select_page = browser.find_element_by_css_selector("#block-H .mod-album_tab_num a.selected")
+    i = 2 if select_page.get_attribute("data-avlist-page") == "1" else int(select_page.get_attribute("data-avlist-page")) - 1
+    step = 1 if select_page.get_attribute("data-avlist-page") == "1" else -1
+    while True:
+        try:
+            buttons = browser.find_element_by_css_selector("#block-H .mod-album_tab_num a:nth-child(" + str(i) + ")")
+        except:
+            break
+        buttons.click()
+        time.sleep(1)
+        j = 1
+        while True:
+            try:
+                urls = browser.find_element_by_css_selector(".wrapper-piclist ul "
+                                                            "li:nth-child(" + str(j) + ") "
+                                                            ".site-piclist_info "
+                                                            ".site-piclist_info_title a")
+            except:
+                break
+            yield urls.text, urls.get_attribute("href")
+            j += 1
+        i += step
     browser.quit()
