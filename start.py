d302f181af9db286fa37a6a5fb38ac865c9c3b3b
RockJohnson503 <836867547@qq.com>
2018-05-02 13:48:16 +0800

    添加youku_Spider的启动命令

--- start.py
+++ start.py
@@ -9,4 +9,5 @@ import sys
 from scrapy.cmdline import execute
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-execute(["scrapy", "crawl", "qiyi"])
+# execute(["scrapy", "crawl", "qiyi"])
+execute(["scrapy", "crawl", "youku"])
