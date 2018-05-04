2705ee1f20609b107a56518a733ae31f44b8e015
RockJohnson503 <836867547@qq.com>
2018-05-04 16:42:29 +0800

    新增了一些通用的方法

--- tools/common.py
+++ tools/common.py
@@ -4,3 +4,43 @@
 File: common.py
 Author: Rock Johnson
 """
+import hashlib, re, pymysql
+from videoSpider import settings
+
+
+# 将路径加密为长度固定的字符
+def get_md5(url):
+    if isinstance(url, str):
+        url = url.encode("utf-8")
+    m = hashlib.md5()
+    m.update(url)
+    return m.hexdigest()
+
+# 格式化电视剧 动漫和综艺的集数
+def episode_format(episode):
+    try:
+        episode = int(episode)
+        return episode
+    except:
+        return int(re.sub("\D", "", episode))
+
+# 将报错的综艺添加到数据库
+def error_video(list_type, url, name):
+    dbparms = dict(
+        host=settings.MYSQL_HOST,
+        db=settings.MYSQL_DBNAME,
+        user=settings.MYSQL_USER,
+        passwd=settings.MYSQL_PASSWORD,
+        charset='utf8',
+        use_unicode=True,
+    )
+    conn = pymysql.connect(**dbparms)
+    cursor = conn.cursor()
+
+    insert_sql = """
+        insert into errvideos(list_type, video_url, video_name)
+        values(%s, %s, %s)
+    """
+
+    cursor.execute(insert_sql, (list_type, url, name))
+    conn.commit()
