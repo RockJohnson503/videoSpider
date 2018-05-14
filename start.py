25ebc875735478f5c4238c62d2cfdba26183415f
RockJohnson503 <836867547@qq.com>
2018-05-14 16:56:00 +0800

    修改spider的启动器

--- start.py
+++ start.py
@@ -10,17 +10,22 @@ from scrapy.cmdline import execute
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 i = 1
-# while True:
-#     try:
-#         execute(["scrapy", "crawl", "qiyi"])
-#     except Exception as e:
-#         print("爬取错误")
 while True:
     try:
         print("第%s轮爬取开始" % i)
-        execute(["scrapy", "crawl", "youku"])
+        execute(["scrapy", "crawl", "iqiyi"])
         print("第%s轮爬取结束" % i)
-    except:
+    except Exception as e:
         print("爬取错误")
+        print(e)
     i += 1
+# while True:
+#     try:
+#         print("第%s轮爬取开始" % i)
+#         execute(["scrapy", "crawl", "youku"])
+#         print("第%s轮爬取结束" % i)
+#     except Exception as e:
+#         print("爬取错误")
+#         print(e)
+#     i += 1
 # execute(["scrapy", "crawl", "alone_test"])
