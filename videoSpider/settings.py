1a95bfb1833c9fb5f0a9d580a7dde0bdd78874bb
RockJohnson503 <836867547@qq.com>
2018-05-18 16:51:15 +0800

    将初始化的爬取延迟设置为0

--- videoSpider/settings.py
+++ videoSpider/settings.py
@@ -83,7 +83,7 @@ ITEM_PIPELINES = {
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
 AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-AUTOTHROTTLE_START_DELAY = 2
+AUTOTHROTTLE_START_DELAY = 0
 # The maximum download delay to be set in case of high latencies
 AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
