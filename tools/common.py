d23c638cc4f5d76e49a870f53d5d924ad1aaec64
RockJohnson503 <836867547@qq.com>
2018-06-05 15:45:05 +0800

    修改错误视频表的方法及添加记录爬取信息的方法

--- tools/common.py
+++ tools/common.py
@@ -4,31 +4,8 @@
 File: common.py
 Author: Rock Johnson
 """
-import hashlib, re, pymysql
+import hashlib, re, pymysql, datetime
 from videoSpider import settings
-
-
-# 将路径加密为长度固定的字符
-def get_md5(url):
-    if isinstance(url, str):
-        url = url.encode("utf-8")
-    m = hashlib.md5()
-    m.update(url)
-    return m.hexdigest()
-
-# 格式化电视剧 动漫和综艺的集数
-def episode_format(episode):
-    try:
-        episode = int(episode)
-        return episode
-    except:
-        return int(re.sub("\D", "", episode))
-
-# 判断是否为播放页面
-def is_player(url):
-    pat = re.compile(r'.*www.iqiyi.com/v_.*')
-    res = pat.match(url)
-    return res
 
 # 连接mysql数据库
 class conn_sql:
@@ -54,14 +31,36 @@ class conn_sql:
         self.cursor.close()
         self.conn.close()
 
+# 将路径加密为长度固定的字符
+def get_md5(url):
+    if isinstance(url, str):
+        url = url.encode("utf-8")
+    m = hashlib.md5()
+    m.update(url)
+    return m.hexdigest()
+
+# 格式化电视剧 动漫和综艺的集数
+def episode_format(episode):
+    try:
+        episode = int(episode)
+        return episode
+    except:
+        return int(re.sub("\D", "", episode))
+
+# 判断是否为播放页面
+def is_player(url):
+    pat = re.compile(r'.*www.iqiyi.com/v_.*')
+    res = pat.match(url)
+    return res
+
 # 将报错的综艺添加到数据库
-def error_video(list_type, url, name):
+def error_video(list_type, url, reason, name):
     insert_sql = """
-        insert into errvideos(list_type, video_url, video_name)
-        values(%s, %s, %s) on duplicate key update list_type = values(list_type)
+        insert into errvideos(list_type, video_url, reason, video_name, r_time)
+        values(%s, %s, %s, %s, %s) on duplicate key update r_time = values(r_time)
     """
     conn = conn_sql("errors")
-    conn.excute(insert_sql, (list_type, url, name))
+    conn.excute(insert_sql, (list_type, url, reason, name, datetime.datetime.now()))
     conn.close()
 
 # 将报错的语法添加到数据库
@@ -72,4 +71,14 @@ def error_grammer(grammer, sql, param, video_name):
     """
     conn = conn_sql("errors")
     conn.excute(insert_sql, (grammer, sql, param, video_name))
+    conn.close()
+
+# 记录爬取的信息
+def crawl_info(c_name, s_time, e_time, sd_time, ing, ed, err):
+    insert_sql = """
+            insert into info(crawl_name, start_time, end_time, spend_time, crawling, crawled, crawlerr)
+            values(%s, %s, %s, %s, %s, %s, %s)
+        """
+    conn = conn_sql("crawlinfo")
+    conn.excute(insert_sql, (c_name, s_time, e_time, sd_time, ing, ed, err))
     conn.close()
