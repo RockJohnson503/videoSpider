572fb835b80cf884b32d8a1e752a489fc3dd85a3
RockJohnson503 <836867547@qq.com>
2018-06-26 19:37:09 +0800

    修改启动方法

--- start.py
+++ start.py
@@ -7,7 +7,7 @@ Author: Rock Johnson
 from datetime import datetime
 from scrapy.cmdline import execute
 
-choose = input("1. 爱奇艺全站爬取\n2. 优酷全站爬取\n3. 爱奇艺定向爬取 \n4. 优酷定向爬取\n5. 运行单独测试\n请选择(默认5): ")
+choose = input("1. 爱奇艺全站爬取\n2. 优酷全站爬取\n3. 爱奇艺定向爬取 \n4. 优酷定向爬取\n5. 腾讯定向爬取\n6. 运行单独测试\n请选择(默认6): ")
 
 if choose == "1":
     where = "iqiyi"
@@ -17,11 +17,13 @@ elif choose == "3":
     where = "iqiyiDir"
 elif choose == "4":
     where = "youkuDir"
+elif choose == "5":
+    where = "tenceDir"
 else:
     execute(["scrapy", "crawl", "alone_test", "-s", "LOG_STDOUT=False"])
 
 try:
-    execute(["scrapy", "crawl", where, "-s", "LOG_FILE=logs/%s_%s.log" % (where, datetime.now())])
+    execute(["scrapy", "crawl", where, "-s", "LOG_FILE=logs/%s_%s.log" % (where, datetime.now().replace(microsecond=0))])
 except Exception as e:
     print("爬取错误")
     print(e)
