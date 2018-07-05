9cc07764d9290a42769b00dcd3754d516e7967e9
RockJohnson503 <836867547@qq.com>
2018-07-05 19:12:27 +0800

    增加请求量,优化代码

--- start.py
+++ start.py
@@ -9,10 +9,13 @@ from scrapy.cmdline import execute
 
 choose = input("1. 爱奇艺全站爬取\n2. 优酷全站爬取\n3. 爱奇艺定向爬取 \n4. 优酷定向爬取\n5. 腾讯定向爬取\n6. 运行单独测试\n请选择(默认6): ")
 
+apd = None
 if choose == "1":
-    where = "iqiyi -s CONCURRENT_REQUESTS=200"
+    where = "iqiyi"
+    apd = ["-s", "CONCURRENT_REQUESTS=250"]
 elif choose == "2":
-    where = "youku -s CONCURRENT_REQUESTS=200"
+    where = "youku"
+    apd = ["-s", "CONCURRENT_REQUESTS=250"]
 elif choose == "3":
     where = "iqiyiDir"
 elif choose == "4":
@@ -23,7 +26,11 @@ else:
     execute(["scrapy", "crawl", "alone_test", "-s", "LOG_STDOUT=False"])
 
 try:
-    execute(["scrapy", "crawl", where, "-s", "LOG_FILE=logs/%s_%s.log" % (where, datetime.now().replace(microsecond=0))])
+    time = str(datetime.now().replace(microsecond=0)).replace(" ", "_")
+    cmd = ["scrapy", "crawl", where, "-s", "LOG_FILE=logs/%s_%s.log" % (where, time)]
+    if apd:
+        cmd += apd
+    execute(cmd)
 except Exception as e:
     print("爬取错误")
     print(e)
