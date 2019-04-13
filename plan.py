b5b3ae7ac0ce5f420626f22e2cb6065ddd9441f6
RockJohnson503 <836867547@qq.com>
2019-04-13 18:03:33 +0800

    新增计划任务

--- plan.py
+++ plan.py
@@ -5,27 +5,27 @@ File: plan.py
 Author: Rock Johnson
 """
 from threading import Timer
-import os, datetime
+import os, datetime, time
 
 i = 1
 num = 0
+wait = 0
 
 def plan_run(where):
     global i
+    if num == 0:
+        t = Timer(wait, plan_run, [where])
+        t.start()
+    if i < num:
+        i += 1
+        t = Timer(wait, plan_run, [where])
+        t.start()
 
     cmd = "scrapy crawl %s -s LOG_FILE=logs/%s_%s.log"
     if where == "tenceDir":
         cmd += " -s AUTOTHROTTLE_START_DELAY=1"
     time = str(datetime.datetime.now().replace(microsecond=0)).replace(" ", "_")
     os.system(cmd % (where, where, time))
-
-    if num == 0:
-        t = Timer(10, plan_run, [where])
-        t.start()
-    if i < num:
-        i += 1
-        t = Timer(10, plan_run, [where])
-        t.start()
 
 
 if __name__ == '__main__':
@@ -37,4 +37,8 @@ if __name__ == '__main__':
     else:
         where = "tenceDir"
     num = int(input("爬取几次(0表示无限): "))
+    if num != 1:
+        wait = int(input("间隔时间(单位为秒, 默认0秒): "))
+    delay = int(input("延迟时间(单位为秒): "))
+    time.sleep(delay)
     plan_run(where)
